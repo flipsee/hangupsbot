552ab8f86dc585a3a1fb0af961185bff47d95c73
flipsee <kn1ght7id@yahoo.com>
2016-09-23 16:09:34 +0800

    added support for nltk

--- hangupsbot/commands/__init__.py
+++ hangupsbot/commands/__init__.py
@@ -1,5 +1,5 @@
+from functools import wraps
 import os, glob, asyncio
-
 
 class CommandDispatcher:
     """Register commands and run them"""
@@ -13,11 +13,61 @@ class CommandDispatcher:
         commands_admin = bot.get_config_suboption(conv_id, 'commands_admin') or []
         return list(set(commands_admin + self.commands_admin))
 
+    def __scorecommands(self, commandname, keyword=[]):
+        print("Command Received: " + str(commandname) + ", Keyword Received: " + str(keyword))
+
+        __commandName = commandname
+        __highestScore = 0
+
+        #if keyword == None: keyword = []
+        keyword.append(commandname)
+        keyword = set(keyword)
+
+        for func_name in self.commands:
+            func_keyword = []
+            print(func_name + " Tags: " + str(self.commands[func_name][1]))
+            func_keyword = self.commands[func_name][1]
+            func_keyword.append(func_name)
+            print(func_name + " After append: " + str(func_keyword))
+            func_keyword = set(func_keyword)
+            
+            # commandName is same with func_name: +2
+            # func_name is in the command_keyword: +1
+            # commandName is in the func_keyword: +1
+            # func_keyword intersect command_keyword: +1
+            __commandScore = 0
+            if commandname == func_name: __commandScore = __commandScore + 1
+            
+            #print("Comparing: " + str(keyword) + " with " + str(func_keyword))
+            for kw in list(keyword & func_keyword):
+            # since the commandname and the funcname is added to keyword list if it is the
+            # same it actualy add +2 because the statement above.
+                __commandScore =  __commandScore + 1
+
+            print(func_name + " Score: " +str(__commandScore))
+
+            if __highestScore < __commandScore:
+                __commandName = func_name
+                __highestScore = __commandScore
+
+        print("Highest Score: " + str(__commandName))
+        return __commandName
+
     @asyncio.coroutine
-    def run(self, bot, event, *args, **kwds):
-        """Run command"""
+    def run(self, bot, event, keyword=[], *args, **kwds):
+        """Run command
+           since the command now has a keyword we need to do a scoring
+           run the command with the highest score.
+           1. loop all commands 
+           2. calculate all score
+           3. run highest score.
+           4. the parameter ?
+        """
         try:
-            func = self.commands[args[0]]
+            #func = self.commands[args[0]][0]
+            #func_keyword = self.commands[args[0]][1]   
+            func_name = self.__scorecommands(args[0],keyword)      
+            func = self.commands[func_name][0]
         except KeyError:
             if self.unknown_command:
                 func = self.unknown_command
@@ -30,13 +80,17 @@ class CommandDispatcher:
             yield from func(bot, event, *args, **kwds)
         except Exception as e:
             print(e)
-
-    def register(self, *args, admin=False):
-        """Decorator for registering command"""
+      
+    def register(self, *args, admin=False, keyword=[]):
+        """Decorator for registering command
+           added keyword array of the command keyword.
+        """
         def wrapper(func):
             # Automatically wrap command function in coroutine
             func = asyncio.coroutine(func)
-            self.commands[func.__name__] = func
+            if keyword == None: keyword [func.__name__]
+            print("Registering: " + func.__name__ + " Key: " + str(keyword))
+            self.commands[func.__name__] = [func,keyword]
             if admin:
                 self.commands_admin.append(func.__name__)
             return func
